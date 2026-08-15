c0e0ff05eb0573d2ac2e04486a5a10e634b6949d
KYO297 <133432596+KYO297@users.noreply.github.com>
2026-08-15 19:41:17 +0200

    First testing

--- src/main/java/com/KYO297/UniversalBulkCell/UniversalBulkCell.java
+++ src/main/java/com/KYO297/UniversalBulkCell/UniversalBulkCell.java
@@ -14,7 +14,7 @@ import net.minecraftforge.registries.RegistryObject;
 
 @Mod(UniversalBulkCell.MODID)
 public class UniversalBulkCell {
-    public static final String MODID = "UniversalBulkCell";
+    public static final String MODID = "universalbulkcell";
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
     public static final RegistryObject<Item> CELL = ITEMS.register("universal_bulk_cell", () -> new BulkCellItem(new Item.Properties()));
     public static final RegistryObject<Item> HOUSING = ITEMS.register("universal_bulk_cell_housing", () -> new Item(new Item.Properties()));
